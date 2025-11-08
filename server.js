5e2fcd9db7f66614d5e82ec8bfb0364ebaea55e7
Muhammad Arslan <arslanrajput3373@gmail.com>
2025-11-08 20:43:28 +0500

    first commit

--- server.js
+++ server.js
@@ -46,3 +46,8 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
+process.on('unhandledRejection', (err) => {
+  console.error(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
